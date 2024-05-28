1cf2bd685afc226fae722334a781e0ec6e2c9fcf
Santiago Peña Nieto <santip2357@gmail.com>
2024-05-27 21:41:43 -0500

    Ahora los enemigos aparecen de forma aleatoria alrededor del personaje

--- MainScene.gd
+++ MainScene.gd
@@ -5,3 +5,19 @@ extends Node
 func _ready():
 	pause_menu.hide()
 
+func spawn_melee_enemy():
+	var new_mob = preload("res://MeleeEnemy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+	
+func spawn_range_enemy():
+	var new_mob = preload("res://RangeEnemy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout():
+	spawn_melee_enemy()
+	spawn_range_enemy()
